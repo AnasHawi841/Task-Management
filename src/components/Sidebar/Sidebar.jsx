8b60b8aa2e670169856d0dda56092c002676d0ba
Anas <anas.moh.alhawi@gmail.com>
2024-12-25 13:44:10 +0300

    Style sidebar layout

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,7 +2,35 @@ import React from "react";
 import "./Sidebar.css";
 
 const Sidebar = () => {
-  return <aside className="sidebar"></aside>;
+  return (
+    <aside className="sidebar">
+      <div className="sidebar_title">Filter</div>
+      <div className="sidebar_section">
+        <h4 className="sidebar_titleSection">Completion Status</h4>
+        {["All", "Completed", "InComplete"].map((status) => {
+          return (
+            <div className="completion_filter">
+              <input type="radio" name="choice" value={status} />
+              <span>{status}</span>
+            </div>
+          );
+        })}
+      </div>
+      <div className="sidebar_section">
+        <h4 className="sidebar_titleSection">Categories</h4>
+        {["All", "Category 01", "Category 02", "Category 03"].map(
+          (category) => {
+            return (
+              <div className="completion_filter">
+                <input type="radio" name="choice" value={category} />
+                <span>{category}</span>
+              </div>
+            );
+          }
+        )}
+      </div>
+    </aside>
+  );
 };
 
 export default Sidebar;
